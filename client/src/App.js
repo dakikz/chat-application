f99ac2589acbd892ca7c920e70f265a6c9094c42
dakikz <leonidoupetros@gmail.com>
2022-03-14 18:22:09 +0200

    Added fonts

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,14 @@ import ChatWindow from "./components/ChatWindow";
 const socket = io.connect("http://localhost:3010");
 
 const AppOuter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   border: 1px solid red;
+  width: 100%;
+  height: 100vh;
 `;
+const ChatSection = styled.div``;
 const ChatLogin = styled.div``;
 
 function App() {
@@ -24,19 +30,21 @@ function App() {
 
   return (
     <AppOuter>
-      {!showChat ? (
-        <ChatLogin>
-          <h1>Chat Application</h1>
-          <input
-            type="text"
-            placeholder="Enter your username"
-            onChange={(event) => setUsername(event.target.value)}
-          />
-          <button onClick={joinChat}>Join Chat</button>
-        </ChatLogin>
-      ) : (
-        <ChatWindow socket={socket} username={username} />
-      )}
+      <ChatSection>
+        {!showChat ? (
+          <ChatLogin>
+            <h1>Chat Application</h1>
+            <input
+              type="text"
+              placeholder="Enter your username"
+              onChange={(event) => setUsername(event.target.value)}
+            />
+            <button onClick={joinChat}>Join Chat</button>
+          </ChatLogin>
+        ) : (
+          <ChatWindow socket={socket} username={username} />
+        )}
+      </ChatSection>
     </AppOuter>
   );
 }
